6a4ed99d7a49d79889dfd52ee522b725f149a557
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-13 08:59:29 +0800

    api configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    }
+  },
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: 'index.html',
+    }
+  }
 })
