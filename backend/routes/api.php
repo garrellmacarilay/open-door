94ca5d94bcbba3a4dce6356f8499670c56eeb721
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-16 15:22:31 +0800

    changes in redirectoring

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\GoogleController;
 use App\Http\Controllers\Api\CalendarController;
 
 Route::get('/user', function (Request $request) {
@@ -11,5 +13,12 @@ Route::get('/user', function (Request $request) {
 // Route::middleware(['auth:sanctum'])->group(function () {
 //     Route::get('/calendar/events', [CalendarController::class, 'index']);
 // });
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/auth/google/', [GoogleController::class, 'redirect']); 
+    Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
+});
+
+
 
 Route::get('/calendar/events', [CalendarController::class, 'index']);
