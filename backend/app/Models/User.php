bea3905d2abc52ce5873bbd52b33c8403e39bb79
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-16 15:20:39 +0800

    configuration in user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use App\Models\Student;
 use App\Models\Staff;
 use App\Models\Admin;
@@ -14,7 +15,7 @@ use App\Models\EmailNotification;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens ,HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
