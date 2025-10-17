dbb5c7078343ba6957e7240fea36f3f21715bbcf
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-17 21:54:57 +0800

    changes in the routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\GoogleController;
 use App\Http\Controllers\Api\CalendarController;
+use App\Http\Controllers\Api\BookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,10 +16,25 @@ Route::get('/user', function (Request $request) {
 // });
 Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/auth/google/', [GoogleController::class, 'redirect']); 
+    Route::get('/auth/google/', [GoogleController::class, 'redirect']);
     Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
 });
 
 
+Route::middleware(['auth:sanctum'])->group(function() {
+    // Protected routes for authenticated users
+});
+
+Route::middleware(['auth:sanctum', 'staff'])->group(function () {
+    // Protected routes for staff users
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    // Protected routes for admin users
+});
+
+
+
 
 Route::get('/calendar/events', [CalendarController::class, 'index']);
+Route::post('/bookings', [BookingController::class, 'store']);
