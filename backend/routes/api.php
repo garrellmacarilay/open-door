8dd8890a54466925bd340b2cd1d627202f70158f
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-10-28 19:49:52 +0800

    changes in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\GoogleController;
-use App\Http\Controllers\Api\CalendarController;
 use App\Http\Controllers\Api\BookingController;
+use App\Http\Controllers\Api\CalendarController;
+use App\Http\Controllers\Api\NotificationController;
+use App\Http\Controllers\Api\Admin\AdminBookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,11 +17,13 @@ Route::get('/user', function (Request $request) {
 // Route::middleware(['auth:sanctum'])->group(function () {
 //     Route::get('/calendar/events', [CalendarController::class, 'index']);
 // });
+
 Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/auth/google', [GoogleController::class, 'redirect']);
     Route::get('/auth/google/callback', [GoogleController::class, 'callback']);
 });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return response()->json([
@@ -34,6 +39,9 @@ Route::middleware(['auth:sanctum'])->group(function() {
     Route::get('/bookings/history', [BookingController::class, 'history']);
     Route::get('/bookings/recent', [BookingController::class, 'recent']);
 
+    Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::get('/notifications/read/{id}', [NotificationController::class, 'markAsRead']);
+
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
@@ -42,7 +50,9 @@ Route::middleware(['auth:sanctum', 'staff'])->group(function () {
 });
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    // Protected routes for admin users
+    Route::get('/admin/dashboard', [AdminBookingController::class, 'dashboard']);
+    Route::get('/bookings', [AdminBookingController::class, 'index']);
+    Route::patch('/bookings/{id}/status', [AdminBookingController::class, 'updateStatus']);
 });
 
 
