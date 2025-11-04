ae6bb4b572d71e99aa09c19638adacee470d1b75
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-04 15:11:57 +0800

    changes in routing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Office;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -8,8 +9,8 @@ use App\Http\Controllers\Api\GoogleController;
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\CalendarController;
 use App\Http\Controllers\Api\NotificationController;
+use App\Http\Controllers\Api\Admin\AdminEventController;
 use App\Http\Controllers\Api\Admin\AdminBookingController;
-use App\Models\Office;
 
 
 Route::get('/user', function (Request $request) {
@@ -35,19 +36,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware(['auth:sanctum'])->group(function() {
-    Route::get('/calendar/events', [CalendarController::class, 'index']);
+    Route::get('/calendar/appointments', [CalendarController::class, 'index']);
     Route::get('/offices', function() {
         return response()->json(Office::all());
     });
-
-    Route::post('/bookings', [BookingController::class, 'store']);
-    Route::get('/bookings/history', [BookingController::class, 'history']);
-    Route::get('/bookings/recent', [BookingController::class, 'recent']);
+    Route::get('/calendar/events', [AdminEventController::class, 'events']);
 
     Route::get('/notifications', [NotificationController::class, 'index']);
     Route::get('/notifications/read/{id}', [NotificationController::class, 'markAsRead']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum', 'student'])->group(function() {
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::get('/bookings/history', [BookingController::class, 'history']);
+    Route::get('/bookings/recent', [BookingController::class, 'recent']);
+
 });
 
 Route::middleware(['auth:sanctum', 'staff'])->group(function () {
@@ -58,6 +63,7 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminBookingController::class, 'dashboard']);
     Route::get('/bookings', [AdminBookingController::class, 'index']);
     Route::patch('/bookings/{id}/status', [AdminBookingController::class, 'updateStatus']);
+    Route::post('/admin/events', [AdminEventController::class, 'storeEvents']);
 });
 
 
