c6a9fd3b529d5bef08e44795ebaa49ec7066334e
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2025-11-08 06:48:11 +0800

    added resources for clean coding

---
+++ backend/app/Http/Resources/BookingResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookingResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'student_name' => $this->student->user->full_name ?? "Unknown",
+            'office_name' => $this->office->office_name ?? "Unknown",
+            'service_type' => $this->service_type,
+            'consultation_date' => $this->consultation_date,
+            'attached_files' => $this->uploaded_file_url ?? null,
+            'status' => ucfirst($this->status),
+            'color' => $this->getStatusColor($this->status),
+        ];
+    }
+}
