6a50726506145b4e9ed8a965d1cb3f157fb69ade
GarrellMac <gmacarilay1@gmail.com>
2025-11-17 05:40:15 +0800

    Global/Add/Route/Admin&Staff

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ProtectedRoutes from './ProtectedRoutes.jsx'
 import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import StudentContainer from '../modules/Student/pages/Dashboard/StudentContainer.jsx';
+import StaffContainer from '../modules/Staffs/pages/Dashboard/StaffContainer.jsx';
+import AdminContainer from '../modules/Admin_PSAS/pages/Dashboard/AdminContainer.jsx';
 import Landingpage from "../public-pages/Landing/Landingpage.jsx";
 import Login from "../public-pages/Auth/Login.jsx";
 
@@ -13,6 +15,8 @@ function AppRoutes() {
             <Route path="/" element={<Landingpage />}/>
             <Route path="/login" element={<Login />}/>
             <Route path="/dashboard" element={<StudentContainer />} />
+            <Route path="/staff-dashboard" element={<StaffContainer />} />
+            <Route path="/admin-dashboard" element={<AdminContainer />} />
         </Routes>
     </BrowserRouter>
 
