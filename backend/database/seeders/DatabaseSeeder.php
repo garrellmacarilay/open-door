72cd35c566ed23fe5956e135aff0135c90e42c1e
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-04-10 16:10:29 +0800

    initialize db seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             OfficeSeeder::class,
             BookingSeeder::class,
-            StaffSeeder::class,
+            AdminSeeder::class,
+            StaffSeeder::class
         ]);
     }
 }
